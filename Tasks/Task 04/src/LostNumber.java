51ae76e3b8b1a1283603c7e2b5aa7aeb9d1cdcf7
EugenyaS <39557015+EugenyaS@users.noreply.github.com>
2018-12-21 10:01:52 +0300

    complit Task 04

---
+++ Tasks/Task 04/src/LostNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LostNumber {
+    public static void main(String[] args) {
+        int sumAllNumber=0;
+        // считаем сумму всех чисел от 1 до 1000
+        for (int i = 1; i<=1000; i++){
+            sumAllNumber+=i;
+        }
+        // ввод чисел и вычисление пропущенного
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i < 1000; i++) {
+            number = scanner.nextInt();
+            sumAllNumber -= number;
+        }
+        System.out.print(sumAllNumber);
+    }
+}
