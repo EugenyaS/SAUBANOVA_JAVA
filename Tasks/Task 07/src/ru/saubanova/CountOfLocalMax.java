aab83627baffd5e3992766a00c10f312be6be4ef
Eugenya <water1983@yandex.ru>
2018-12-21 22:49:30 +0300

    correct boolean operator Task 07

--- Tasks/Task 07/src/ru/saubanova/CountOfLocalMax.java
+++ Tasks/Task 07/src/ru/saubanova/CountOfLocalMax.java
@@ -8,7 +8,7 @@ public class CountOfLocalMax {
         int arr[] = {123,22,34,35,11,34,56,78,2,3,3,4,1};
         int cntLocalMax = 0;
         for (int i = 1; i < arr.length-2; i++) {
-            if (arr[i-1]<arr[i] & arr[i]<arr[i+1]){
+            if (arr[i-1]<arr[i] && arr[i]<arr[i+1]){
                 cntLocalMax++;
             }
         }
